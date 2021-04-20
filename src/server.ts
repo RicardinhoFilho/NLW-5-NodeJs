2401fcadc4a9a046fe7def74cc9096d7d74ced7b
Ricardo <ricardinhogiasson16@gmail.com>
2021-04-20 14:21:10 -0300

    Criando banco de dados e migração Settings

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,17 @@
 import express, { Response, Request } from "express";
 
+import "./database/index";
+
 const app = express();
 
+app.get("/", (req: Request, res: Response) => {
+  const msg = "Olá NLW 05";
+  return res.json({ msg });
+});
 
-/*  GET= Buscas
-    POST = Criação
-    PUT = Alteração
-    DELETED = Deletar
-    PATCH = Alterar informação expecífica
-*/
-app.get("/", (req:Request, res:Response)=>{
-    const msg = "Olá NLW 05";
-    return res.json({msg})
-})
+app.post("/", (req: Request, res: Response) => {
+  const msg = "Rota Post";
+  return res.json({ msg });
+});
 
-app.post("/", (req:Request, res:Response)=>{
-    const msg = "Rota Post";
-    return res.json({msg})
-})
-
-app.listen(3333,()=>console.log("Server is running on port 3333"))
-
+app.listen(3333, () => console.log("Server is running on port 3333"));
